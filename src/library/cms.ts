4a96be4908b9b0505a49efded1e5daab12dbf09b
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-11-01 23:43:41 +0900

    fix: cms types

--- src/library/cms.ts
+++ src/library/cms.ts
@@ -1,4 +1,10 @@
-import { createClient } from "newt-client-js";
+import { createClient, Content } from "newt-client-js";
+
+interface Article extends Content {
+  title: string;
+  content: string;
+  date: string;
+}
 
 const client = createClient({
   spaceUid: import.meta.env.NEWT_SPACE_UID,
@@ -7,7 +13,7 @@ const client = createClient({
 });
 
 export const getArticles = async () => {
-  return await client.getContents({
+  return await client.getContents<Article>({
     appUid: "portfolio",
     modelUid: "article",
     query: {
