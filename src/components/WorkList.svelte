85cbc64a514eff19c1ee7634134f7243960249d4
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-11-29 00:12:13 +0900

    fix: WorkList

--- src/components/WorkList.svelte
+++ src/components/WorkList.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
   import type { Work } from '../library/useCms';
+
   export let works: Work[];
-  console.log(works);
 </script>
 
 <ul class="grid grid-cols-2 gap-x-6 gap-y-8">
   {#each works as work (work._id)}
     <li>
-      <div class="aspect-video w-full rounded-lg bg-slate-300" />
-      <h3 class="mt-2 text-xl font-bold">{work.title}</h3>
-      <p class="mt-1 text-sm text-slate-600">Webデザイン/実装</p>
+      <img
+        src={work['main-image'].src}
+        alt={work['main-image'].altText}
+        width={work['main-image'].width}
+        height={work['main-image'].height}
+        class="aspect-video w-full rounded-lg bg-slate-300 shadow-md"
+      />
+      <h3 class="mt-3 text-xl font-bold">{work.title}</h3>
+      <ul class="mt-1 flex gap-2">
+        {#each work.category as category}
+          <li
+            class="rounded-md bg-slate-300 px-2 py-0.5 font-barlow text-sm font-semibold tracking-wider"
+          >
+            {category}
+          </li>
+        {/each}
+      </ul>
     </li>
   {/each}
 </ul>
