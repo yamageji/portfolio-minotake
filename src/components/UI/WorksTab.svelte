d5e86fbba79c957871d96f49b8e39be142786eba
K-shigehito <example@test.com>
2022-12-13 13:49:30 +0900

    fix: style

--- src/components/UI/WorksTab.svelte
+++ src/components/UI/WorksTab.svelte
@@ -1,18 +1,60 @@
 <script lang="ts">
+  import WorkList from '@components/WorkList.svelte';
+  import type { Work } from '@library/useCms';
+
   type TabList = {
     id: string;
     title: string;
-    content: [
-      {
-        url: string;
-        title: string;
-      }
-    ];
+    content: Work[] | undefined;
   };
 
   export let data: TabList[];
 
+  const tabs: HTMLButtonElement[] = [];
   let activeIndex = 0;
+  let focusedIndex: number;
+  const tabsLastIndex = data.length - 1;
+
+  const onFocusTab = (index: number) => {
+    focusedIndex = index;
+  };
+  const onKeydownTab = (event: KeyboardEvent) => {
+    const key = event.key;
+    switch (key) {
+      case 'ArrowRight':
+      case 'Right': // for IE, Edge 16-, Firefox 36-
+        focusNextTab();
+        break;
+      case 'ArrowLeft':
+      case 'Left': // for IE, Edge 16-, Firefox 36-
+        focusPreviousTab();
+        break;
+      case 'Home':
+        focusTab(0);
+        break;
+      case 'End':
+        focusTab(tabsLastIndex);
+        break;
+    }
+  };
+
+  const focusTab = (index: number) => {
+    tabs[index].focus();
+  };
+  const focusNextTab = () => {
+    let nextIndex = focusedIndex + 1;
+    if (nextIndex > tabsLastIndex) {
+      nextIndex = 0;
+    }
+    focusTab(nextIndex);
+  };
+  const focusPreviousTab = () => {
+    let nextIndex = focusedIndex - 1;
+    if (nextIndex < 0) {
+      nextIndex = tabsLastIndex;
+    }
+    focusTab(nextIndex);
+  };
 
   const onClickTab = (index: number) => {
     if (activeIndex === index) return;
@@ -20,49 +62,51 @@
   };
 </script>
 
-<div>
+<div class="w-full">
   <ul
-    class="flex gap-6 font-barlow-semi text-3xl font-semibold"
     role="tablist"
-    aria-label="works"
+    aria-label="works category link"
+    class="flex gap-6 border-b-2 border-slate-300 font-barlow-semi text-3xl font-semibold text-slate-400"
   >
     {#each data as { id, title }, index}
       {@const currentActive = activeIndex === index}
       {@const onClick = () => onClickTab(index)}
-      <li class="" role="presentation">
+      {@const onFocus = () => onFocusTab(index)}
+      <li role="presentation" class="-mb-0.5">
         <button
           type="button"
-          class="px-4 py-2 tracking-wider"
           id="tab-{id}"
+          bind:this={tabs[index]}
+          on:focus={onFocus}
+          on:keydown={onKeydownTab}
           role="tab"
           aria-controls="panel-{id}"
           aria-selected={currentActive ? 'true' : 'false'}
           tabindex={currentActive ? 0 : -1}
           on:click={onClick}
+          class="relative block px-2 py-2 tracking-wider hover:text-slate-900 focus:text-slate-900 aria-selected:border-b-2 aria-selected:border-teal-500 aria-selected:text-slate-900"
         >
           {title}
         </button>
       </li>
     {/each}
   </ul>
-  <div class="">
+  <div class="mt-10 w-full">
     {#each data as { id, title, content }, index}
       {@const currentActive = activeIndex === index}
       <div
-        aria-hidden={currentActive ? 'false' : 'true'}
-        class=""
         id="panel-{id}"
         role="tabpanel"
+        aria-hidden={currentActive ? 'false' : 'true'}
         aria-labelledby="tab-{id}"
         tabindex="0"
+        class="aria-hidden:hidden"
       >
-        <ol class="">
-          {#each content as { url, title }}
-            <li class="-item">
-              <a href={url} class="">{title}</a>
-            </li>
-          {/each}
-        </ol>
+        {#if content && content.length !== 0}
+          <WorkList works={content} />
+        {:else}
+          <div>コンテンツがありません</div>
+        {/if}
       </div>
     {/each}
   </div>
