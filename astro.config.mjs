3aadfe4d05f4b7eb5f7aa303e9c4229b13152e76
K-shigehito <example@test.com>
2023-01-10 16:10:19 +0900

    fix: style

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
 
 // const SITE_URL = '';
 
 export default defineConfig({
   // site: SITE_URL,
-  integrations: [tailwind(), svelte(), sitemap()],
+  integrations: [
+    tailwind(),
+    svelte(),
+    sitemap(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
