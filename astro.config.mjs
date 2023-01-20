80cac5ebf5a6e149bf4716934b285849080e8f69
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2023-01-21 00:28:18 +0900

    feat: site-map and  robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,13 @@ import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
+import robotsTxt from "astro-robots-txt";
+const SITE_URL = 'https://minotake.dev/';
 
-// const SITE_URL = '';
-
+// https://astro.build/config
 export default defineConfig({
-  // site: SITE_URL,
-  integrations: [
-    tailwind(),
-    svelte(),
-    sitemap(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-  ],
+  site: SITE_URL,
+  integrations: [tailwind(), svelte(), sitemap(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), robotsTxt()]
 });
