802b9b015d12be92147ba552dbdca20780a26a10
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2023-01-21 23:40:57 +0900

    fix: tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -21,26 +20,32 @@ module.exports = {
       },
     },
     colors: {
-      white: '#ffffff',
-      'bg-primary': '#E2E8F0', //slate-200
-      'bg-secondary': '#334155', //slate-700
-      'bg-surface-primary': '#F8FAFC', //teal-50
-      'bg-surface-secondary': '#0f172a', //slate-900
-      'bg-accent-primary': '#14b8a6', //teal-500
-      'bg-accent-secondary': '#94A3B8', //teal-400
-
-      'text-primary': '#0f172a', //slate-900
-      'text-secondary': '#334155', //slate-700
-      'text-tertiary': '#475569', //slate-600
-      'text-quaternary': '#1e293b', //slate-800
-      'text-light': '#f8fafc', //slate-800
-      'text-accent-primary': '#0d9488', //teal-600
-
-      'border-primary': '#e2e8f0', //slate-200
-      'border-secondary': '#64748b', //slate-500
-      'border-tertiary': '#334155', //slate-700
-      'border-quaternary': '#CBD5E1', //slate-300
-      'border-accent-primary': '#0d9488', //teal-600
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      bg: {
+        primary: '#E2E8F0', //slate-200
+        secondary: '#334155', //slate-700
+        'surface-primary': '#F8FAFC', //teal-50
+        'surface-secondary': '#0f172a', //slate-900
+        'accent-primary': '#14b8a6', //teal-500
+        'accent-secondary': '#94A3B8', //teal-400
+      },
+      text: {
+        primary: '#0f172a', //slate-900
+        secondary: '#334155', //slate-700
+        tertiary: '#475569', //slate-600
+        quaternary: '#1e293b', //slate-800
+        light: '#f8fafc', //slate-800
+        'accent-primary': '#0d9488', //teal-600
+      },
+      border: {
+        primary: '#e2e8f0', //slate-200
+        secondary: '#64748b', //slate-500
+        tertiary: '#334155', //slate-700
+        quaternary: '#CBD5E1', //slate-300
+        'accent-primary': '#0d9488', //teal-600
+      },
     },
   },
   plugins: [require('@tailwindcss/container-queries')],
