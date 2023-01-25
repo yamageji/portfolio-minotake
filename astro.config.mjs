b28eafd8f8c552e0110eb25361ee16c0153c50d9
K-shigehito <example@test.com>
2023-01-26 08:39:18 +0900

    fix: robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@ import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
-import robotsTxt from "astro-robots-txt";
-const SITE_URL = 'https://minotake.dev/';
+import robotsTxt from 'astro-robots-txt';
+const SITE_URL = 'https://minotake.dev';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [tailwind(), svelte(), sitemap(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), robotsTxt()]
+  integrations: [
+    tailwind(),
+    svelte(),
+    sitemap(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    robotsTxt(),
+  ],
 });
