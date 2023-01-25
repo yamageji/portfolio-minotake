fcb69fa6a1ad258dc78f1808daee613e8a2b6998
K-shigehito <example@test.com>
2023-01-25 14:37:21 +0900

    fix: og image

---
+++ src/library/constants.ts
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: 'minotake',
+  siteDescription: 'Portfolio of Koyama Shigehito',
+  siteUrl: 'https://minotake.dev',
+  siteLang: 'ja',
+  siteLocale: 'ja_JP',
+  siteType: 'website',
+  siteIcon: '/favicon.svg',
+};
