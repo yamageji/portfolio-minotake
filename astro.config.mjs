cad060a3de1e410d646c8029b3e2d1edebf742cf
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2023-05-12 23:10:27 +0900

    fix: Image integration to Assets #16

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
-import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 const SITE_URL = 'https://minotake.dev';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [
-    tailwind(),
-    svelte(),
-    sitemap(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    robotsTxt(),
-  ],
+  integrations: [tailwind(), svelte(), sitemap(), robotsTxt()],
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: sharpImageService(),
+  },
 });
